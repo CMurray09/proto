73282c9156db2bf68c99a3ae425caca90fa16f5a
Cameron M <cameronmurray@hotmail.ca>
2022-09-02 20:41:44 +0100

    Home component, header with login button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "src/app/user/login/login.component";
+import {HomeComponent} from "src/app/home/home.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'login',
     component: LoginComponent
