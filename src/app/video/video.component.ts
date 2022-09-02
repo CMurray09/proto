670a2d4a10e06754cc76802095c6d84fbf8e022b
Cameron M <cameronmurray@hotmail.ca>
2022-09-02 21:19:16 +0100

    Default thumbnail add & no thumbnail fix

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -13,8 +13,23 @@ export class VideoComponent implements OnInit {
   constructor(public videoService: VideoService) { }
 
   ngOnInit(): void {
+    // Normally would not use any type, but time constraints from solving this issue
     this.videoService.getVideos()
       .subscribe((videos: any) => {
+        for (const video of videos) {
+          if (!video['images']) {
+            video['images'] = [
+              {
+                url: '/assets/default-thumbnail.png',
+                type: 'thumbnail'
+              },
+              {
+                url: '/assets/default-thumbnail.png',
+                type: 'packshot'
+              },
+            ]
+          }
+        }
         this.videos = videos;
       })
   }
